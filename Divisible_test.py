b6c167a49c65b91fc3911c958300cd342846de7f
Vishnutk2001 <t.k.vishnu2001@gmail.com>
2022-03-28 12:37:21 +0530

    completed

--- Divisible_test.py
+++ Divisible_test.py
@@ -2,22 +2,23 @@ import Divisible
 
 import pytest
 
-def test_divisible_5():
+@pytest.fixture
+def input():
     x = 20
-    res = Divisible.divisible_5(x)
+    return x
+
+def test_divisible_5(input):
+    res = Divisible.divisible_5(input)
     assert res == True
 
-def test_divisible_7():
-    x = 14
-    res1 = Divisible.divisible_7(x)
+def test_divisible_7(input):
+    res1 = Divisible.divisible_7(input)
     assert res1 == True
 
-def test_divisible_9():
-    x = 18
-    res2 = Divisible.divisible_9(x)
+def test_divisible_9(input):
+    res2 = Divisible.divisible_9(input)
     assert res2 == True
 
-def test_divisible_10():
-    x = 20
-    res3 = Divisible.divisible_10(x)
+def test_divisible_10(input):
+    res3 = Divisible.divisible_10(input)
     assert res3 == True
